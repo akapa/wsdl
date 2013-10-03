942b4c80678e2576d761c956b67a771b6073e0e3
Bazmega Kapa <ricsi@expattech.com>
2013-10-03 18:26:59 +0200

    Used the Factory for an example.

--- js/main.js
+++ js/main.js
@@ -14,17 +14,17 @@ requirejs(['underscore', 'wsconfig'], function (_, wsconfig) {
 	console.log(service);
 	var reqObj = service.factory.make('getEventsInRange');
 	service.call('getEventsInRange', reqObj);
+	var ev = service.factory.make('event');
 	var test = {
 		test1: true,
 		multis: [1,2,3.5,Infinity],
-		obj: {
-			classify: function () { return 'event'; },
+		obj: _(ev).extend({
 			amount: 10.5,
 			description: 'fasza',
 			id: 56,
 			time: new Date(),
 			type: 'xy'
-		},
+		}),
 		classify: function () { return 'testObject'; }
 	};
 	var testType = {
