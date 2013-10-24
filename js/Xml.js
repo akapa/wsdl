8f87d96c52034fdb17bc54a9f82685901d31b4fa
Bazmega Kapa <ricsi@expattech.com>
2013-10-24 20:16:44 +0200

    Xml.parse method added.

--- js/Xml.js
+++ js/Xml.js
@@ -21,6 +21,18 @@ define(function () {
 		},
 		getHeader : function () {
 			return '<?xml version="1.0" encoding="UTF-8"?>';
+		},
+		parse: function (s) {
+			if (typeof window.DOMParser != "undefined") {
+		        return ( new window.DOMParser() ).parseFromString(s, "text/xml");
+			} else if (typeof window.ActiveXObject != "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
+			    var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+			    xmlDoc.async = "false";
+			    xmlDoc.loadXML(s);
+			    return xmlDoc;
+			} else {
+			    throw new Error("No XML parser found");
+			}
 		}
 	};
 	return Xml;
