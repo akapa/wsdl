786ed46279405749d5586d5e7c61f65d2c928db8
Bazmega Kapa <ricsi@expattech.com>
2013-10-25 17:03:06 +0200

    Changed the way namespaces are stored - maybe not final.

--- js/wsconfig.js
+++ js/wsconfig.js
@@ -1,8 +1,10 @@
 define(['underscore', 'objTools', 'WebService', 'TypeLibrary', 'TypeDefinition', 
 	'MethodLibrary', 'MethodDefinition', 'SoapSerializer', 'Factory'], 
 function (_, objTools, WebService, TypeLibrary, TypeDefinition, MethodLibrary, MethodDefinition, SoapSerializer, Factory) {
-	var ns = 'http://budget.kapa.org';
-	var schemaNs = 'http://www.w3.org/2001/XMLSchema';
+	var namespaces = {
+		0: 'http://budget.kapa.org',
+		'xs': 'http://www.w3.org/2001/XMLSchema'
+	};
 	var url = 'Service';
 
 	//PROTO OBJECTS FOR XSD COMPLEX TYPES
@@ -39,34 +41,34 @@ function (_, objTools, WebService, TypeLibrary, TypeDefinition, MethodLibrary, M
 	var types = [
 		objTools.make(TypeDefinition, {
 			type: 'event',
-			ns: ns,
+			ns: namespaces['tns'],
 			complex: true,
 			constructorFunction: function Event () {
 				return objTools.construct(objects.event, Event);
 			},
 			properties: {
 				'amount': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'float'
 				}),
 				'description': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'string'
 				}),
 				'id': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'int'
 				}),
 				'time': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'dateTime'
 				}),
 				'type': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'string'
 				}),
 				'user': objTools.make(TypeDefinition, {
-					ns: ns,
+					ns: namespaces['tns'],
 					complex: true,
 					type: 'user'
 				})
@@ -74,56 +76,56 @@ function (_, objTools, WebService, TypeLibrary, TypeDefinition, MethodLibrary, M
 		}),
 		objTools.make(TypeDefinition, {
 			type: 'user',
-			ns: ns,
+			ns: namespaces['tns'],
 			complex: true,
 			constructorFunction: function User () {
 				return objTools.construct(objects.user, User);
 			},
 			properties: {
 				'events': objTools.make(TypeDefinition, {
-					ns: ns,
+					ns: namespaces['tns'],
 					type: 'event',
 					multiple: true,
 					complex: true
 				}),
 				'id': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'int'
 				}),
 				'name': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'string'
 				})
 			}
 		}),
 		objTools.make(TypeDefinition, {
 			type: 'getEventsInRange',
-			ns: ns,
+			ns: namespaces['tns'],
 			complex: true,
 			constructorFunction: function GetEventsInRange () {
 				return objTools.construct(objects.getEventsInRange, GetEventsInRange);
 			},
 			properties: {
 				'timeFrom': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'dateTime'
 				}),
 				'timeTo': objTools.make(TypeDefinition, {
-					ns: schemaNs,
+					ns: namespaces['xs'],
 					type: 'dateTime'
 				})
 			}
 		}),
 		objTools.make(TypeDefinition, {
 			type: 'getEventsInRangeResponse',
-			ns: ns,
+			ns: namespaces['tns'],
 			complex: true,
 			constructorFunction: function getEventsInRangeResponse () {
 				return objTools.construct(objects.getEventsInRangeResponse, getEventsInRangeResponse);
 			},
 			properties: {
 				'return': objTools.make(TypeDefinition, {
-					ns: ns,
+					ns: namespaces['tns'],
 					complex: true,
 					multiple: true,
 					type: 'event'
@@ -157,7 +159,8 @@ function (_, objTools, WebService, TypeLibrary, TypeDefinition, MethodLibrary, M
 			name: 'getEventsInRange',
 			requestObject: 'getEventsInRange',
 			responseObject: 'getEventsInRangeResponse',
-			endpoint: url
+			endpoint: url,
+			namespaces: namespaces
 		})
 	];
 
