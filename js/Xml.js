5963e908aab44641c02f9ac1ac77c44942a826ec
Bazmega Kapa <ricsi@expattech.com>
2013-10-28 15:34:56 +0100

    Moved Xml formatting to Xml.js

--- js/Xml.js
+++ js/Xml.js
@@ -58,6 +58,38 @@ define(function () {
 				}
 			});
 			return doc;
+		},
+		format: function (xml) {
+		    var formatted = '';
+		    var reg = /(>)(<)(\/*)/g;
+		    xml = xml.replace(reg, '$1\r\n$2$3');
+		    var pad = 0;
+
+		    _.each(xml.split('\r\n'), function(node, index) {
+		        var indent = 0;
+		        if (node.match( /.+<\/\w[^>]*>$/ )) {
+		            indent = 0;
+		        }
+		        else if (node.match( /^<\/\w/ )) {
+		            if (pad != 0) {
+		                pad -= 1;
+		            }
+		        }
+		        else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+		            indent = 1;
+		        }
+		        else {
+		            indent = 0;
+		        }
+		        var padding = '';
+		        for (var i = 0; i < pad; i++) {
+		            padding += '  ';
+		        }
+		        formatted += padding + node + '\r\n';
+		        pad += indent;
+		    });
+
+		    return formatted;
 		}
 	};
 	return Xml;
--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ requirejs.config({
 	}
 });
 
-requirejs(['underscore', 'wsconfig'], function (_, wsconfig) {
+requirejs(['underscore', 'wsconfig', 'Xml'], function (_, wsconfig, Xml) {
 	var service = wsconfig.service;
 	console.log(service);
 
@@ -44,41 +44,8 @@ requirejs(['underscore', 'wsconfig'], function (_, wsconfig) {
 	});
 
 	var xml = service.serializer.serialize(resp, 'getEventsInRangeResponse');
-	document.getElementById('show').innerText = formatXml(xml);
+	document.getElementById('show').innerText = Xml.format(xml);
 
 	var td = service.typeLibrary.getItem('getEventsInRangeResponse');
 	console.log(td, service.serializer.unserialize(xml, 'getEventsInRangeResponse', td));
 });
-
-function formatXml(xml) {
-    var formatted = '';
-    var reg = /(>)(<)(\/*)/g;
-    xml = xml.replace(reg, '$1\r\n$2$3');
-    var pad = 0;
-
-    _.each(xml.split('\r\n'), function(node, index) {
-        var indent = 0;
-        if (node.match( /.+<\/\w[^>]*>$/ )) {
-            indent = 0;
-        }
-        else if (node.match( /^<\/\w/ )) {
-            if (pad != 0) {
-                pad -= 1;
-            }
-        }
-        else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
-            indent = 1;
-        }
-        else {
-            indent = 0;
-        }
-        var padding = '';
-        for (var i = 0; i < pad; i++) {
-            padding += '  ';
-        }
-        formatted += padding + node + '\r\n';
-        pad += indent;
-    });
-
-    return formatted;
-}
