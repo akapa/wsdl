c23f463171f12e0139d278f9df60d97a2543bcc0
Bazmega Kapa <ricsi@expattech.com>
2014-01-14 16:56:09 +0100

    Some refactoring in validators.

--- js/ComplexTypeNodeValidator.js
+++ js/ComplexTypeNodeValidator.js
@@ -4,7 +4,7 @@ function (_, objTools, Xml, NodeValidator, XmlValidationResult, XmlValidationErr
 	var complexTypeNodeValidator = objTools.make(NodeValidator, {
 		validate: function () {
 			var xsdNow = this.getFirstElement(this.definition);
-			var xmlNow, occurLimit, validator, childXsd;
+			var xmlNow, occurLimit;
 			var errors = [];
 			do {
 				//collecting XML nodes that are to be validated by the current XSD node
@@ -20,48 +20,25 @@ function (_, objTools, Xml, NodeValidator, XmlValidationResult, XmlValidationErr
 
 				//calling the right validators for all nodes
 				if (xmlNow.length) {
-					childXsd = this.findTypeDefFromNodeAttr(xsdNow, 'type');
-					validator = this.validatorFactory.getValidator(childXsd, xmlNow[0]);
-					console.log(xmlNow[0], childXsd);
-					_(xmlNow).each(function (elem) {
-						validator.node = elem;
-						var result = validator.validate();
-						if (!result.success) {
-							errors.concat(result.errors);
-						}
-					});
+					errors.concat(this.callChildValidators(xmlNow, xsdNow));
 				}
 
 			} while (xsdNow = this.getNextElement(xsdNow));
 
 			return new XmlValidationResult(errors);
 		},
-		findTypeDefFromNodeAttr: function (node, typeAttr) {
-			var type = node.getAttribute(typeAttr);
-			var parts = type.split(':');
-			var ns = node.lookupNamespaceURI(parts[0]);
-			var def = this.validatorFactory.xsdLibrary.findXsdDefinition(ns, parts[1]);
-			return (!def && ns === Xml.xs) ? parts[1] : def;
-		},
-		parseMinMaxOccurs: function (xsdNode) {
-			var min = xsdNode.getAttribute('minOccurs');
-			if (min === null || min === '') {
-				min = 1;
-			}
-			else {
-				min = parseInt(min, 10);
-			}
-			var max = xsdNode.getAttribute('maxOccurs');
-			if (max === null || max === '') {
-				max = 1;
-			}
-			else if (max === 'unbounded') {
-				max = Infinity;
-			}
-			else {
-				max = parseInt(max, 10);
-			}
-			return { min: min, max: max	};
+		callChildValidators: function (xmlNodes, xsdNode) {
+			var errors = [];
+			var typeDef = this.findTypeDefFromNodeAttr(xsdNode, 'type');
+			var validator = this.validatorFactory.getValidator(typeDef, xmlNodes[0]);
+			_(xmlNodes).each(function (elem) {
+				validator.node = elem;
+				var result = validator.validate();
+				if (!result.success) {
+					errors.concat(result.errors);
+				}
+			});
+			return errors;
 		},
 		getFirstElement: function (xsdNode) {
 			var elems = xsdNode.getElementsByTagNameNS(xsdNode.namespaceURI, 'element');
