2f5b3cfc8692c48918ca5fd374c4b34b807d53b9
Kapa <kapa@sipaksi.net>
2014-04-01 17:10:33 +0200

    Changed dependencies to point to GitHub.

--- js/lib/xml.js
+++ js/lib/xml.js
@@ -1,6 +1,21 @@
 define(function () {
 
-	var xml = {
+	/**
+	 * @namespace xml
+	 * @desc A set of useful methods for XML DOM manipulation/traversal. Can be used as a compatibility layer.
+	 */
+	var xml = 
+	/**
+	 * @lends xml
+	 */
+	{
+		/**
+		 * Create a new XML DOM document.
+		 * @param {string} name - The element name for the main element in the document (for example `schema`).
+		 * @param {Object.<string, string>} namespaces - The namespaces to define on the main element. They key is short notation (like `xs`), the value is the namespace URI.
+		 * @param {string} [prefix] - The prefix to be used before the main element's name (for example `xs` to get `xs:schema`).
+		 * @returns {Document}
+		 */
 		createDocument: function (name, namespaces, prefix) {
 			var qname = prefix ? [prefix, name].join(':') : name;
 			var doc = document.implementation.createDocument(namespaces[0], qname, null);
@@ -13,30 +28,68 @@ define(function () {
 			});
 			return doc;
 		},
+		/**
+		 * Parse an XML string to a DOM document.
+		 * @param {string} s - A string containing valid XML.
+		 * @returns {Document}
+		 */
 		parseToDom: function (s) {
 			if (typeof window.DOMParser != "undefined") {
-		        return (new window.DOMParser()).parseFromString(s, "text/xml");
+				return (new window.DOMParser()).parseFromString(s, "text/xml");
 			} else if (typeof window.ActiveXObject != "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
-			    var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-			    xmlDoc.async = "false";
-			    xmlDoc.loadXML(s);
-			    return xmlDoc;
+				var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+				xmlDoc.async = "false";
+				xmlDoc.loadXML(s);
+				return xmlDoc;
 			} else {
-			    throw new Error("No XML parser found");
+				throw new Error("No XML parser found.");
 			}
 		},
+		/**
+		 * Serialize a DOM into an XML string.
+		 * @param {Element} dom - A DOM element/document.
+		 * @returns {string} The XML representation of the given DOM.
+		 */
 		serializeToString: function (dom) {
 			return new XMLSerializer().serializeToString(dom);
 		},
+		/**
+		 * Returns the `textContent` of the given node.
+		 * @param {Element} node - A DOM element.
+		 * @returns {string}
+		 */
 		getNodeText: function (node) {
 			return node.textContent;
 		},
+		/**
+		 * Sets the `textContent` of the given node.
+		 * @param {Element} node - A DOM element.
+		 * @param {string} value - The new text content.
+		 */
 		setNodeText: function (node, value) {
 			node.textContent = value;
 		},
+		/**
+		 * Returns the closest ancestor of the given node with the given namespace and name.
+		 * @param {Element} node - A DOM element.
+		 * @param {string} namespace - The namespace URI to look for.
+		 * @param {string} tagname - The tag name to look for.
+		 * @returns {Element|null}
+		 */
 		getClosestAncestor: function (node, namespace, tagname) {
-			var node = node.parentElement;
-			while (!(node.namespaceURI === namespace && node.localName === tagname)) {
+			return this.getFirstFilteredAncestor(node, function () {
+				return node.namespaceURI === namespace && node.localName === tagname;
+			});
+		},
+		/**
+		 * Returns the closest ancestor of the given node that satisfies a filter function.
+		 * @param {Element} node - A DOM element.
+		 * @param {Function} filterFunction - The filter function that will run for every ancestor. Receives the current node as a parameter, returning a truthy value will stop the search and return the current node.
+		 * @returns {Element|null}
+		 */
+		getFirstFilteredAncestor: function (node, filterFunction) {
+			node = node.parentElement;
+			while (!filterFunction(node)) {
 				node = node.parentElement;
 				if (!node) {
 					return null;
@@ -44,37 +97,45 @@ define(function () {
 			}
 			return node;
 		},
-		formatString: function (xml, spaces) {
-		    xml = xml.replace(/(>)(<)(\/*)/g, '$1\r\n$2$3');
-		    spaces = spaces || 4;
-		    var formatted = '';
-		    var pad = 0;
+		/**
+		 * Formats an XML string to be readable by humans, adding identation.
+		 * @param {string} xml - A valid XML string.
+		 * @param {number} [spaces=4] - The number of spaces to use for one level of indentation.
+		 * @returns {string}
+		 */
+		formatString: function (str, spaces) {
+			str = str.replace(/(>)(<)(\/*)/g, '$1\n$2$3');
+			spaces = spaces || 4;
+			var formatted = '';
+			var pad = 0;
 
-		    _.each(xml.split('\r\n'), function(node, index) {
-		        var indent = 0;
-		        var padding = '';
-		        if (node.match( /.+<\/\w[^>]*>$/ )) {
-		            indent = 0;
-		        }
-		        else if (node.match( /^<\/\w/ )) {
-		            if (pad != 0) {
-		                pad -= 1;
-		            }
-		        }
-		        else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
-		            indent = 1;
-		        }
-		        else {
-		            indent = 0;
-		        }
-		        for (var i = 0; i < pad; i++) {
-		            padding += new Array(spaces).join(' ');
-		        }
-		        formatted += padding + node + '\r\n';
-		        pad += indent;
-		    });
+			_.each(str.split('\n'), function(node, index) {
+				node = node.trim();
+				if (!node) return;
+				var indent = 0;
+				var padding = '';
+				if (node.match( /.+<\/\w[^>]*>$/ )) {
+					indent = 0;
+				}
+				else if (node.match( /^<\/\w/ )) {
+					if (pad !== 0) {
+						pad -= 1;
+					}
+				}
+				else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+					indent = 1;
+				}
+				else {
+					indent = 0;
+				}
+				for (var i = 0; i < pad; i++) {
+					padding += new Array(spaces).join(' ');
+				}
+				formatted += padding + node + '\n';
+				pad += indent;
+			});
 
-		    return formatted;
+			return formatted;
 		}
 	};
 
